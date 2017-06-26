e9817207e2f61007767037bad29c3b4ee3ee576e
caxieyou <248500947@qq.com>
2017-06-26 16:59:25 +0800

    MOD-develop #refactory, wrap gl

--- src/effect.js
+++ src/effect.js
@@ -1,25 +1,7 @@
-// Vertex shader program
-var VSHADER_SOURCE = v_shader_main;
 
+var effectRender = new EffectRender();
 
-// Fragment shader program
-var FSHADER_SOURCE = f_shader_header    + 
-                     f_uniform_list     +
-                     f_varying_list     +
-                     f_func_list        +
-                     f_func_main;
-
-//Uniform 变量名集合
-var uniformNameSet = ["u_SampleImage",      "u_SamplerCurve",   "u_SamplerColorBalance", 
-                      "u_Brightness",       "u_Contrast", 
-                      "u_Hue",              "u_Saturation",     "u_Lightness", 
-                      "u_InputMinStage",    "u_InputMaxStage",  "u_Gamma",              "u_OutputMinStage", "u_OutputMaxStage", 
-                      "u_Sharpen",          "u_InvSize",
-                      "u_PreserveLuminosity", "u_Halo"];
-//Uniform 集合
-var uniformSet = [];
-
-var gl;     //webgl context
+//var gl;     //webgl context
 var colorBalanceSetting = {
     cyan    : 0,
     magenta : 0,
@@ -30,78 +12,67 @@ var colorBalanceSetting = {
 function onBrightnessChanged(value)
 {
     document.getElementById("brightness").innerHTML = value;
-    gl.uniform1f(uniformSet['u_Brightness'], value);
-    updateCanvas();
+    effectRender.setUniform1f('u_Brightness', value);
 }
 
 function onContrastChanged(value)
 {
     document.getElementById("contrast").innerHTML = value;
-    gl.uniform1f(uniformSet['u_Contrast'], value);
-    updateCanvas();
+    effectRender.setUniform1f('u_Contrast', value);
 }
 
 function onHueChanged(value)
 {
     document.getElementById("hue").innerHTML = value;
-    gl.uniform1f(uniformSet['u_Hue'], value);
-    updateCanvas();
+    effectRender.setUniform1f('u_Hue', value);
 }
 
 function onSaturationChanged(value)
 {
     document.getElementById("saturation").innerHTML = value;
-    gl.uniform1f(uniformSet['u_Saturation'], value);
-    updateCanvas();
+    effectRender.setUniform1f('u_Saturation', value);
 }
 
 function onLightnessChanged(value)
 {
     document.getElementById("lightness").innerHTML = value;
-    gl.uniform1f(uniformSet['u_Lightness'], value);
-    updateCanvas();
+    effectRender.setUniform1f('u_Lightness', value);
 }
 
 function onInputStageMinChanged(value)
 {
     document.getElementById("inputMinStage").innerHTML = value;
-    gl.uniform1f(uniformSet['u_InputMinStage'], value);
-    updateCanvas();
+    effectRender.setUniform1f('u_InputMinStage', value);
 }
 
 function onInputStageMaxChanged(value)
 {
     document.getElementById("inputMaxStage").innerHTML = value;
-    gl.uniform1f(uniformSet['u_InputMaxStage'], value);
-    updateCanvas();
+    effectRender.setUniform1f('u_InputMaxStage', value);
 }
 
 function onGammaChanged(value)
 {
     document.getElementById("gamma").innerHTML = value;
-    gl.uniform1f(uniformSet['u_Gamma'], value);
-    updateCanvas();
+    effectRender.setUniform1f('u_Gamma', value);
 }
 
 function onOutputStageMinChanged(value)
 {
     document.getElementById("outputMinStage").innerHTML = value;
-    gl.uniform1f(uniformSet['u_OutputMinStage'], value);
-    updateCanvas();
+    effectRender.setUniform1f('u_OutputMinStage', value);
 }
 
 function onOutputStageMaxChanged(value)
 {
     document.getElementById("outputMaxStage").innerHTML = value;
-    gl.uniform1f(uniformSet['u_OutputMaxStage'], value);
-    updateCanvas();
+    effectRender.setUniform1f('u_OutputMaxStage', value);
 }
     
 function onSharpenChanged(value)
 {
     document.getElementById("sharpen").innerHTML = value;
-    gl.uniform1f(uniformSet['u_Sharpen'], value);
-    updateCanvas();
+    effectRender.setUniform1f('u_Sharpen', value);
 }
 
 function onCyanChanged(value)
@@ -109,11 +80,7 @@ function onCyanChanged(value)
     document.getElementById("cyan").innerHTML = value;
     colorBalanceSetting.cyan = value;
     var colorBalanceTable = pre_colorBalance(colorBalanceSetting);
-    gl.activeTexture(gl.TEXTURE2);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, 256, 1, 0, gl.RGB, gl.UNSIGNED_BYTE, colorBalanceTable);
-
-    updateCanvas();
-    
+    effectRender.setColorBalance(colorBalanceTable);
     return colorBalanceTable;
 }
 
@@ -122,11 +89,7 @@ function onMagentaChanged(value)
     document.getElementById("magenta").innerHTML = value;
     colorBalanceSetting.magenta = value;
     var colorBalanceTable = pre_colorBalance(colorBalanceSetting);
-    gl.activeTexture(gl.TEXTURE2);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, 256, 1, 0, gl.RGB, gl.UNSIGNED_BYTE, colorBalanceTable);
-
-    updateCanvas();
-    
+    effectRender.setColorBalance(colorBalanceTable);
     return colorBalanceTable;
 }
 
@@ -135,30 +98,27 @@ function onYellowChanged(value)
     document.getElementById("yellow").innerHTML = value;
     colorBalanceSetting.yellow = value;
     var colorBalanceTable = pre_colorBalance(colorBalanceSetting);
-    gl.activeTexture(gl.TEXTURE2);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, 256, 1, 0, gl.RGB, gl.UNSIGNED_BYTE, colorBalanceTable);
-
-    updateCanvas();
+    effectRender.setColorBalance(colorBalanceTable);
     return colorBalanceTable;
 }
 
 function onHaloChanged(value)
 {
     document.getElementById("halo").innerHTML = value;
-    gl.uniform1f(uniformSet['u_Halo'], value / 100);
-    updateCanvas();
+    effectRender.setUniform1f('u_Halo', value);
 }
 
-
-//刷新页面，绘制canvas
-function updateCanvas() {
-    gl.clear(gl.COLOR_BUFFER_BIT);   // Clear <canvas>
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4); // Draw the rectangle
-    //强制刷新，异步改为同步
-    var syncBuffer = new Uint8Array(4); 
-    gl.readPixels(0, 0, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, syncBuffer);
+//var points = [];
+//points[0] = {x: 0, y: 0};
+//points[1] = {x: 255, y: 255};
+//var curveTable = pre_applyCurve(points);
+//Points是个数组，每个含有x和y变量，都在0-255之间，x顺序从小到大排列
+function onCurveChanged(points) {
+    var curveTable = pre_applyCurve(points);
+    //this._createTexture(1, curveTable);
+    effectRender.setCurve(curveTable);
+    return curveTable;
 }
-
 
 function hist(image) {
     
@@ -186,7 +146,6 @@ function hist(image) {
             
             var grayValue =  Math.floor(r * 0.299 + g * 0.587 + b * 0.114);
             //debugger;
-            //console.log(grayValue);
             histCount[grayValue]++;
         }
     }
@@ -197,190 +156,11 @@ function hist(image) {
     return histCount;
 }
 
-function reset(setting) {
-    if(setting) {
-        /*
-        gl.uniform1f(uniformSet['u_Brightness'], setting.brightness);
-        gl.uniform1f(uniformSet['u_Contrast'], setting.contrast);
-        ...
-        //设置曲线的颜色映射
-        gl.activeTexture(gl.TEXTURE1);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, 256, 1, 0, gl.RGB, gl.UNSIGNED_BYTE, setting.curveMap);
-        
-        //设置色彩平衡的颜色映射
-        gl.activeTexture(gl.TEXTURE2);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, 256, 1, 0, gl.RGB, gl.UNSIGNED_BYTE, setting.colorBalanceMap);
-        */
-        
-    } else {
-        gl.uniform1f(uniformSet['u_Brightness'], 0);
-        gl.uniform1f(uniformSet['u_Contrast'], 0);
-        gl.uniform1f(uniformSet['u_Hue'], 0);
-        gl.uniform1f(uniformSet['u_Saturation'], 0);
-        gl.uniform1f(uniformSet['u_Lightness'], 0);
-        gl.uniform1f(uniformSet['u_Sharpen'], 0);
-        gl.uniform1f(uniformSet['u_InputMinStage'], 0);
-        gl.uniform1f(uniformSet['u_InputMaxStage'], 255);
-        gl.uniform1f(uniformSet['u_Gamma'], 1);
-        gl.uniform1f(uniformSet['u_OutputMinStage'], 0);
-        gl.uniform1f(uniformSet['u_OutputMaxStage'], 255);
-        gl.uniform1f(uniformSet['u_Halo'], 0);
-        
-        var resetMap = new Uint8Array(256 * 3);
-        for (var i = 0; i < 256; i++) {
-            resetMap[i * 3] = i;
-            resetMap[i * 3 + 1] = i;
-            resetMap[i * 3 + 2] = i;
-        }
-        
-        //设置曲线的颜色映射
-        gl.activeTexture(gl.TEXTURE1);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, 256, 1, 0, gl.RGB, gl.UNSIGNED_BYTE, resetMap);
-        
-        //设置色彩平衡的颜色映射
-        gl.activeTexture(gl.TEXTURE2);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, 256, 1, 0, gl.RGB, gl.UNSIGNED_BYTE, resetMap);
-        
-    }
-    
-    updateCanvas();
-}
-
-
 function main() {
     // 获取Canvas
     var canvas = document.getElementById('webgl');
 
-    init(canvas);
-}
-
-function init(canvas) {
-    if (gl === undefined) {
-        gl = getWebGLContext(canvas);
-        if (!gl) {
-            console.log('Failed to get the rendering context for WebGL');
-            return;
-        }
-
-        // 初始化shader
-        initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
-
-        // 初始化点
-        _initVertexBuffers();
-
-        // Specify the color for clearing <canvas>
-        gl.clearColor(1.0, 1.0, 1.0, 1.0);
-
-        _initUniforms();
-        
-        _initTextures();//加载图片
-    }
-}
-
-function _initVertexBuffers() {
-    var verticesTexCoords = new Float32Array([
-        // Vertex coordinates, texture coordinate
-        -1.0,  1.0,   0.0, 1.0,
-        -1.0, -1.0,   0.0, 0.0,
-         1.0,  1.0,   1.0, 1.0,
-         1.0, -1.0,   1.0, 0.0,
-    ]);
-
-    // Create the buffer object
-    var vertexTexCoordBuffer = gl.createBuffer();
-
-    // Bind the buffer object to target
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexTexCoordBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, verticesTexCoords, gl.STATIC_DRAW);
-
-    var FSIZE = verticesTexCoords.BYTES_PER_ELEMENT;
-    //Get the storage location of a_Position, assign and enable buffer
-    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-
-    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 4, 0);
-    gl.enableVertexAttribArray(a_Position);  // Enable the assignment of the buffer object
-
-    // Get the storage location of a_TexCoord
-    var a_TexCoord = gl.getAttribLocation(gl.program, 'a_TexCoord');
-
-    // Assign the buffer object to a_TexCoord variable
-    gl.vertexAttribPointer(a_TexCoord, 2, gl.FLOAT, false, FSIZE * 4, FSIZE * 2);
-    gl.enableVertexAttribArray(a_TexCoord);  // Enable the assignment of the buffer object
-}
-
-function _initUniforms() {
-    for(var i = 0; i < uniformNameSet.length; i++) {
-        uniformSet[uniformNameSet[i]] = gl.getUniformLocation(gl.program, uniformNameSet[i]);
-        if (!uniformSet[uniformNameSet[i]]) {
-            console.log('Failed to get the storage location of ' + 'uniformNameSet[i]');
-            return false;
-        }
-    }
-    
-    gl.uniform1i(uniformSet['u_SampleImage'], 0);
-    gl.uniform1i(uniformSet['u_SamplerCurve'], 1);
-    gl.uniform1i(uniformSet['u_SamplerColorBalance'], 2);
-    gl.uniform1f(uniformSet['u_Brightness'], 0);
-    gl.uniform1f(uniformSet['u_Contrast'], 0);
-    gl.uniform1f(uniformSet['u_Hue'], 0);
-    gl.uniform1f(uniformSet['u_Saturation'], 0);
-    gl.uniform1f(uniformSet['u_Lightness'], 0);
-    gl.uniform1f(uniformSet['u_Sharpen'], 0);
-    gl.uniform1i(uniformSet['u_PreserveLuminosity'], 1);
-    gl.uniform1f(uniformSet['u_InputMinStage'], 0);
-    gl.uniform1f(uniformSet['u_InputMaxStage'], 255);
-    gl.uniform1f(uniformSet['u_Gamma'], 1);
-    gl.uniform1f(uniformSet['u_OutputMinStage'], 0);
-    gl.uniform1f(uniformSet['u_OutputMaxStage'], 255);
-    gl.uniform1f(uniformSet['u_Halo'], 0);
-}
-
-function _createTexture(index, colorTable, image) {
-    var texture =  gl.createTexture();   // Create a texture object
-    if (!texture) {
-        console.log('Failed to create the texture object');
-        return false;
-    }
-    
-    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); // Flip the image's y axis
-    gl.activeTexture(gl.TEXTURE0 + index);
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-
-    // Set the texture parameters
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-        
-    if (colorTable) {
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, 256, 1, 0, gl.RGB, gl.UNSIGNED_BYTE, colorTable);
-    }
-    if (image) {
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
-        gl.uniform2f(uniformSet['u_InvSize'], 1 / image.width, 1/ image.height);
-    }
-    
-}
-
-function switchImage(image) {
-    reset();
-    gl.activeTexture(gl.TEXTURE0);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
-    gl.uniform2f(uniformSet['u_InvSize'], 1 / image.width, 1/ image.height);
-    updateCanvas();
-}
-
-function _initTextures() {
-    //初始化曲线
-    var points = [];
-    points[0] = {x: 0, y: 0};
-    points[1] = {x: 255, y: 255};
-    var curveTable = pre_applyCurve(points);
-    _createTexture(1, curveTable);
-    
-    //初始化色彩平衡
-    var colorBalanceTable = pre_colorBalance(colorBalanceSetting);
-    _createTexture(2, colorBalanceTable);
+    effectRender.init(canvas);
     
     var image = new Image();  // Create the image object
     if (!image) {
@@ -390,12 +170,8 @@ function _initTextures() {
     
     // Register the event handler to be called on loading an image
     image.onload = function(){ 
-                    //创建图片纹理
-                    _createTexture(0, null, image);
-                    updateCanvas(); 
+                    effectRender.setSrcImage(image);
                  };
     // Tell the browser to load an image
     image.src = '../resources/view.jpg';
-
-    return true;
 }
---
+++ src/render.js
@@ -0,0 +1,212 @@
+
+
+function EffectRender() {
+    this._gl             = null;
+    this._vshader_source = v_shader_main;
+    this._fshader_source = f_shader_header    + 
+                           f_uniform_list     +
+                           f_varying_list     +
+                           f_func_list        +
+                           f_func_main;
+    this._uniformNameSet = ["u_SampleImage",      "u_SamplerCurve",   "u_SamplerColorBalance", 
+                            "u_Brightness",       "u_Contrast", 
+                            "u_Hue",              "u_Saturation",     "u_Lightness", 
+                            "u_InputMinStage",    "u_InputMaxStage",  "u_Gamma",              "u_OutputMinStage", "u_OutputMaxStage", 
+                            "u_Sharpen",          "u_InvSize",
+                            "u_PreserveLuminosity", "u_Halo"];
+    this._uniformSet     = [];
+    this._srcImg         = null;
+};
+
+EffectRender.prototype.init = function(canvas) {
+    
+    this._gl = getWebGLContext(canvas);
+    this._gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    
+    initShaders(this._gl, this._vshader_source, this._fshader_source);
+    
+    this._initVertexBuffers();
+    this._initUniforms(this._uniformNameSet);
+    this._initTextures();
+};
+
+EffectRender.prototype._initVertexBuffers = function() {
+    var verticesTexCoords = new Float32Array([
+        // Vertex coordinates, texture coordinate
+        -1.0,  1.0,   0.0, 1.0,
+        -1.0, -1.0,   0.0, 0.0,
+         1.0,  1.0,   1.0, 1.0,
+         1.0, -1.0,   1.0, 0.0,
+    ]);
+
+    // Create the buffer object
+    var vertexTexCoordBuffer = this._gl.createBuffer();
+
+    // Bind the buffer object to target
+    this._gl.bindBuffer(this._gl.ARRAY_BUFFER, vertexTexCoordBuffer);
+    this._gl.bufferData(this._gl.ARRAY_BUFFER, verticesTexCoords, this._gl.STATIC_DRAW);
+
+    var FSIZE = verticesTexCoords.BYTES_PER_ELEMENT;
+    //Get the storage location of a_Position, assign and enable buffer
+    var a_Position = this._gl.getAttribLocation(this._gl.program, 'a_Position');
+
+    this._gl.vertexAttribPointer(a_Position, 2, this._gl.FLOAT, false, FSIZE * 4, 0);
+    this._gl.enableVertexAttribArray(a_Position);  // Enable the assignment of the buffer object
+
+    // Get the storage location of a_TexCoord
+    var a_TexCoord = this._gl.getAttribLocation(this._gl.program, 'a_TexCoord');
+
+    // Assign the buffer object to a_TexCoord variable
+    this._gl.vertexAttribPointer(a_TexCoord, 2, this._gl.FLOAT, false, FSIZE * 4, FSIZE * 2);
+    this._gl.enableVertexAttribArray(a_TexCoord);  // Enable the assignment of the buffer object
+};
+
+EffectRender.prototype._initUniforms = function(uniformNameSet) {
+    for(var i = 0; i < uniformNameSet.length; i++) {
+        this._uniformSet[uniformNameSet[i]] = this._gl.getUniformLocation(this._gl.program, uniformNameSet[i]);
+        if (!this._uniformSet[uniformNameSet[i]]) {
+            console.log('Failed to get the storage location of ' + 'uniformNameSet[i]');
+            return false;
+        }
+    }
+    
+    this._gl.uniform1i(this._uniformSet['u_SampleImage'], 0);
+    this._gl.uniform1i(this._uniformSet['u_SamplerCurve'], 1);
+    this._gl.uniform1i(this._uniformSet['u_SamplerColorBalance'], 2);
+    this._gl.uniform1i(this._uniformSet['u_PreserveLuminosity'], 1);
+    this._gl.uniform1f(this._uniformSet['u_Brightness'], 0);
+    this._gl.uniform1f(this._uniformSet['u_Contrast'], 0);
+    this._gl.uniform1f(this._uniformSet['u_Hue'], 0);
+    this._gl.uniform1f(this._uniformSet['u_Saturation'], 0);
+    this._gl.uniform1f(this._uniformSet['u_Lightness'], 0);
+    this._gl.uniform1f(this._uniformSet['u_Sharpen'], 0);
+    this._gl.uniform1f(this._uniformSet['u_InputMinStage'], 0);
+    this._gl.uniform1f(this._uniformSet['u_InputMaxStage'], 255);
+    this._gl.uniform1f(this._uniformSet['u_Gamma'], 1);
+    this._gl.uniform1f(this._uniformSet['u_OutputMinStage'], 0);
+    this._gl.uniform1f(this._uniformSet['u_OutputMaxStage'], 255);
+    this._gl.uniform1f(this._uniformSet['u_Halo'], 0);
+};
+
+EffectRender.prototype._initTextures = function() {
+    //初始化曲线
+    var points = [];
+    points[0] = {x: 0, y: 0};
+    points[1] = {x: 255, y: 255};
+    var curveTable = pre_applyCurve(points);
+    this._createTexture(1, curveTable);
+    
+    //初始化色彩平衡
+    var colorBalanceTable = pre_colorBalance(colorBalanceSetting);
+    this._createTexture(2, colorBalanceTable);
+};
+
+EffectRender.prototype._createTexture = function(index, colorTable, image) {
+    var texture =  this._gl.createTexture();   // Create a texture object
+    if (!texture) {
+        console.log('Failed to create the texture object');
+        return false;
+    }
+    
+    this._gl.pixelStorei(this._gl.UNPACK_FLIP_Y_WEBGL, 1); // Flip the image's y axis
+    this._gl.activeTexture(this._gl.TEXTURE0 + index);
+    this._gl.bindTexture(this._gl.TEXTURE_2D, texture);
+
+    // Set the texture parameters
+    this._gl.texParameteri(this._gl.TEXTURE_2D, this._gl.TEXTURE_MIN_FILTER, this._gl.LINEAR);
+    this._gl.texParameteri(this._gl.TEXTURE_2D, this._gl.TEXTURE_MAG_FILTER, this._gl.LINEAR);
+    this._gl.texParameteri(this._gl.TEXTURE_2D, this._gl.TEXTURE_WRAP_S, this._gl.CLAMP_TO_EDGE);
+    this._gl.texParameteri(this._gl.TEXTURE_2D, this._gl.TEXTURE_WRAP_T, this._gl.CLAMP_TO_EDGE);
+        
+    if (colorTable) {
+        this._gl.texImage2D(this._gl.TEXTURE_2D, 0, this._gl.RGB, 256, 1, 0, this._gl.RGB, this._gl.UNSIGNED_BYTE, colorTable);
+    }
+    if (image) {
+        this._gl.texImage2D(this._gl.TEXTURE_2D, 0, this._gl.RGB, this._gl.RGB, this._gl.UNSIGNED_BYTE, image);
+        this._gl.uniform2f(this._uniformSet['u_InvSize'], 1 / image.width, 1/ image.height);
+    }
+};
+
+EffectRender.prototype.updateCanvas = function() {
+    this._gl.clear(this._gl.COLOR_BUFFER_BIT);   // Clear <canvas>
+    this._gl.drawArrays(this._gl.TRIANGLE_STRIP, 0, 4); // Draw the rectangle
+    //强制刷新，异步改为同步
+    var syncBuffer = new Uint8Array(4); 
+    this._gl.readPixels(0, 0, 1, 1, this._gl.RGBA, this._gl.UNSIGNED_BYTE, syncBuffer);
+};
+
+EffectRender.prototype.setSrcImage = function(image) {
+    if (!this._srcImg) {
+        this._createTexture(0, null, image);
+    } else {
+        this._gl.activeTexture(this._gl.TEXTURE0);
+        this._gl.texImage2D(this._gl.TEXTURE_2D, 0, this._gl.RGB, this._gl.RGB, this._gl.UNSIGNED_BYTE, image);
+    }
+    
+    this._srcImg = image;
+    this._gl.uniform2f(this._uniformSet['u_InvSize'], 1 / image.width, 1/ image.height);
+    this.reset();
+    this.updateCanvas();
+    
+};
+
+EffectRender.prototype.reset = function(setting) {
+    if(setting) {
+        /*
+        this._gl.uniform1f(this._uniformSet['u_Brightness'], setting.brightness);
+        this._gl.uniform1f(this._uniformSet['u_Contrast'], setting.contrast);
+        ...
+        //设置曲线的颜色映射
+        this._gl.activeTexture(this._gl.TEXTURE1);
+        this._gl.texImage2D(this._gl.TEXTURE_2D, 0, this._gl.RGB, 256, 1, 0, this._gl.RGB, this._gl.UNSIGNED_BYTE, setting.curveMap);
+        
+        //设置色彩平衡的颜色映射
+        this._gl.activeTexture(this._gl.TEXTURE2);
+        this._gl.texImage2D(this._gl.TEXTURE_2D, 0, this._gl.RGB, 256, 1, 0, this._gl.RGB, this._gl.UNSIGNED_BYTE, setting.colorBalanceMap);
+        */
+        
+    } else {
+        this._gl.uniform1f(this._uniformSet['u_Brightness'], 0);
+        this._gl.uniform1f(this._uniformSet['u_Contrast'], 0);
+        this._gl.uniform1f(this._uniformSet['u_Hue'], 0);
+        this._gl.uniform1f(this._uniformSet['u_Saturation'], 0);
+        this._gl.uniform1f(this._uniformSet['u_Lightness'], 0);
+        this._gl.uniform1f(this._uniformSet['u_Sharpen'], 0);
+        this._gl.uniform1f(this._uniformSet['u_InputMinStage'], 0);
+        this._gl.uniform1f(this._uniformSet['u_InputMaxStage'], 255);
+        this._gl.uniform1f(this._uniformSet['u_Gamma'], 1);
+        this._gl.uniform1f(this._uniformSet['u_OutputMinStage'], 0);
+        this._gl.uniform1f(this._uniformSet['u_OutputMaxStage'], 255);
+        this._gl.uniform1f(this._uniformSet['u_Halo'], 0);
+        
+        var resetMap = new Uint8Array(256 * 3);
+        for (var i = 0; i < 256; i++) {
+            resetMap[i * 3] = i;
+            resetMap[i * 3 + 1] = i;
+            resetMap[i * 3 + 2] = i;
+        }
+        
+        //设置曲线的颜色映射
+        this._gl.activeTexture(this._gl.TEXTURE1);
+        this._gl.texImage2D(this._gl.TEXTURE_2D, 0, this._gl.RGB, 256, 1, 0, this._gl.RGB, this._gl.UNSIGNED_BYTE, resetMap);
+        
+        //设置色彩平衡的颜色映射
+        this._gl.activeTexture(this._gl.TEXTURE2);
+        this._gl.texImage2D(this._gl.TEXTURE_2D, 0, this._gl.RGB, 256, 1, 0, this._gl.RGB, this._gl.UNSIGNED_BYTE, resetMap);
+        
+    }
+    
+    this.updateCanvas();
+};
+
+EffectRender.prototype.setUniform1f = function(name, value) {
+    this._gl.uniform1f(this._uniformSet[name], value);
+    this.updateCanvas();
+};
+
+EffectRender.prototype.setColorBalance = function(colorBalanceTable) {
+    this._gl.activeTexture(this._gl.TEXTURE2);
+    this._gl.texImage2D(this._gl.TEXTURE_2D, 0, this._gl.RGB, 256, 1, 0, this._gl.RGB, this._gl.UNSIGNED_BYTE, colorBalanceTable);
+    this.updateCanvas();
+};
+
